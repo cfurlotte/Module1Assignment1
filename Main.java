19bcbc1454cd391b80dbd1eb305f0907d8d6fcb7
cfurlotte <cfurlotte@ivytech.edu>
2023-10-26 20:20:13 -0400

    modified:   Main.class 	modified:   Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Test");
+        return;
+    }
+    //Converts from feet to meters
+    public static double footToMeter(double foot) {
+        return 3.279 * foot;
+    }
+
+    //converts from meters to feet
+    public static double meterToFoot(double meter) {
+        return 0.305 * meter;
     }
 }
